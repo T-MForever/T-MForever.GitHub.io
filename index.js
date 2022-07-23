4d84404cc011bc56f8aadc29becd6f9163395d60
Tyler Plihcik <tyler@plihcik.com>
2022-07-23 16:34:48 -0700

    Adding error page, and authentication

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function auth()
+{
 
+    var enteredPassword;
+    var actualPassword = "noice";
+
+    enteredPassword=prompt('Authentication Required, please enter the password. It can be found on the back of your save the date / iinvitation',' ');
+
+    if (actualPassword==enteredPassword)
+    {
+        alert('Authentication Successful, welcome to the party!.');
+    }
+        
+    else
+    {
+        window.location="Error.html";
+    }
+}
