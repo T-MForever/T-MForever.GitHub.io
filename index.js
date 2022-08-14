62d41b200624b0b3136d42492435eb8f625e4e45
Tyler Plihcik <tyler@plihcik.com>
2022-08-14 08:51:12 -0700

    fixing password, tabs, about us  and adding pics

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function auth()
     var enteredPassword;
     var actualPassword = "t&mforever";
 
-    enteredPassword=prompt('Authentication Required, please enter the password. It can be found on the back of your save the date / iinvitation',' ');
+    enteredPassword=prompt('The password can be found on your save the date!');
 
     if (actualPassword==enteredPassword)
     {
